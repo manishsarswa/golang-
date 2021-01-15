0c008b1e4eccdb1933cd4718ee59eccca4a3b015
manishsarswa <76943439+manishsarswa@users.noreply.github.com>
2021-01-15 16:09:27 +0530

    Update task1.go

--- sql/task1.go
+++ sql/task1.go
@@ -1,8 +1,12 @@
 package TestSql
 
+
 import (
 	"database/sql"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
+
 )
 
 type Customer struct {
@@ -19,14 +23,18 @@ type Address struct {
 	customerId int
 }
 
-func getCustomer(db *sql.DB, id int) []Customer {
+func getCustomer(db *sql.DB, id string) []Customer {
 	query:="select * from customer inner join address on customer.ID=address.customer_ID where customer.ID"
 	//sql enjection
 	//sql placeholder
 	var Ids[] interface{}
-	if(id!=0){
+	if(id!="0"){
 		query="select * from customer inner join address on customer.ID=address.customer_ID where customer.ID=?"
-		Ids=append(Ids,id)
+		x,err:=strconv.Atoi(id)
+		if err!=nil{
+
+		}
+		Ids=append(Ids,x)
 	}
 	rows,err:=db.Query(query,Ids...)
 	if err != nil {
@@ -46,3 +54,32 @@ func getCustomer(db *sql.DB, id int) []Customer {
 	}
 	return result
 }
+//func Handler(w http.ResponseWriter,r *http.Request){
+//	db,err:=sql.Open("mysql","root:Manish@123Sharma@/Customer_service")
+//	if err!=nil{
+//		panic(err)
+//	}
+//	s:=r.RequestURI
+//	var x []string=strings.Split(s, "/")
+//	xx,err:=strconv.Atoi(x[1])
+//	rows,err:=db.Query("select * from customer inner join address on customer.ID=address.customer_ID where customer.ID=?", xx)
+//	var result []Customer
+//	for rows.Next(){
+//		var cust Customer
+//		err = rows.Scan(&cust.id, &cust.name, &cust.DOB, &cust.add.id, &cust.add.streetName, &cust.add.city, &cust.add.state, &cust.add.customerId)
+//		if err != nil {
+//			panic(err.Error())
+//		}
+//		//ss=ss+string(cust.ID)
+//		io.WriteString(w,strings.Itoi(&cust.id))
+//
+//
+//		result = append(result, cust)
+//	}
+//
+//}
+//func main(){
+//	http.HandleFunc("/",Handler)
+//	log.Fatal("8080",nil)
+//
+//}
