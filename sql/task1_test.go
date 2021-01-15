8127888a94de7877576ac76749aad31dc41fa6fd
manishsarswa <76943439+manishsarswa@users.noreply.github.com>
2021-01-15 11:41:20 +0530

    Create task1_test.go

---
+++ sql/task1_test.go
@@ -0,0 +1,38 @@
+package TestSql
+
+import (
+	"database/sql"
+
+	"reflect"
+	"testing"
+)
+
+func TestSql(t *testing.T) {
+
+	testcases:=[]struct{
+			input int
+			output []Customer
+	}{
+		{1,[]Customer{{id:1,name:"manish",DOB:"12dec",add:Address{id: 3,streetName: "5th cross",city: "bangalore",state: "karnataka",customerId:1}}}},
+		{2,[]Customer{{id:2,name:"deepika",DOB:"14may",add:Address{id:4,streetName: "5th cross 5th block",city: "bangalore",state: "karnataka",customerId: 2}}}},
+		{0,[]Customer{{id:3,name:"ishan",DOB:"23dec",add:Address{id:1,streetName: "zopsmart",city: "bangalore",state: "karnataka",customerId: 3}},{id:4,name:"sharma",DOB:"2jan",add:Address{id:2,streetName: "zopsmart 24th main",city: "bangalore",state: "karnataka",customerId: 4}},{id:1,name:"manish",DOB:"12dec",add:Address{id: 3,streetName: "5th cross",city: "bangalore",state: "karnataka",customerId:1}},{id:2,name:"deepika",DOB:"14may",add:Address{id:4,streetName: "5th cross 5th block",city: "bangalore",state: "karnataka",customerId: 2}}}},
+		{4555,[]Customer(nil)},
+	}
+	db,err:=sql.Open("mysql","root:Manish@123Sharma@/Customer_service")
+	if err!=nil{
+		panic(err)
+	}
+
+	defer db.Close()
+	for i:=range testcases{
+		id:=testcases[i].input
+
+		stmtOut:=getCustomer(db,id)
+
+		if !reflect.DeepEqual(stmtOut,testcases[i].output){
+			t.Fatalf("FAIL")
+		}
+		t.Logf("PASS")
+
+	}
+}
